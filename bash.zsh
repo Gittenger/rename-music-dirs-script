f510c512311334c6902c3158ed322dc219a7dd2e
gittenger <john.pittenger86@gmail.com>
2021-02-03 15:21:00 -0500

    using js not bash, working tests passed

--- bash.zsh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/zsh
-
-for filename in $1/*; do
-[ -e "$filename" ] || continue
-   rm $filename
-done
--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-const data = [
-	'(1994) bethlehem - Dark Metal',
-	'[1995] For All Tid',
-	'1993 - Dark Medieval Times',
-	'bethlehem - 1996 - Dictius Te Necare cd1',
-	'Satyricon-Nemesis_Divina-1996-DeBT_iNT',
-]
+const path = require('path')
+const fs = require('fs')
 
-const [one, two, three, four, five] = data
-
-const yearRegex = /(\(|\[|\w*)(\d{4})(\)|\]|\w*)[\W]*([A-Z,a-z -_.]*)/
-const year = five.match(yearRegex)[2]
-const rest = five.match(yearRegex)[4]
+const dirname = process.argv[2]
+const regex = /(\(|\[|\w*)(\d{4})(\)|\]|\w*)[\W]*([A-Z,a-z -_.]*)/
+const year = dirname.match(regex)[2]
+const rest = dirname.match(regex)[4]
 
 const result = String.prototype.concat('[', year, '] | ', rest)
 
-console.log(result)
+fs.renameSync(path.resolve(dirname), result)
