d5fcd497b2cf7c945e5e7747b758dcdc0ccc7825
gittenger <john.pittenger86@gmail.com>
2021-02-04 01:03:00 -0500

    notes, working on full func

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const path = require('path')
 const fs = require('fs')
 
 const dirname = process.argv[2]
-const regex = /(\(|\[|\w*)(\d{4})(\)|\]|\w*)[\W]*([A-Z,a-z -_.]*)/
-const year = dirname.match(regex)[2]
-const rest = dirname.match(regex)[4]
+const str = 'Satyricon-Nemesis_Divina-1996-DeBT_iNT'
+// const regex = /(\(|\[|\w*)(\d{4})(\)|\]|\w*)[\W]*([A-Z,a-z -_.]*)/
+
+const year = str.match(regex)[2]
+const rest = str.match(regex)[4]
 
 const result = String.prototype.concat('[', year, '] | ', rest)
 
