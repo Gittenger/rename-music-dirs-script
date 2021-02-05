45f30725475ce433a9891f3391613d3dfc376bb5
gittenger <john.pittenger86@gmail.com>
2021-02-04 22:56:29 -0500

    updated script, now works wherever year is

--- script.js
+++ script.js
@@ -2,25 +2,26 @@ const path = require('path')
 const fs = require('fs')
 
 // path variables for use below
-const albumStr = process.argv[2]
+// const albumStr = process.argv[2]
+const albumStr = 'satyricon -this-_-is-the Album name-2005'
 const albumAbs = path.resolve(albumStr)
 const artistAbs = path.resolve(albumAbs, '..')
 const artistStr = path.basename(artistAbs).toLowerCase()
 
 // check for year
-const yearRegex = /((19|20)\d\d)(.*)/
+const yearRegex = /((19|20)\d\d)/
 let year = ''
 let resultStr = ''
 if (yearRegex.test(albumStr)) {
 	const match = albumStr.match(yearRegex)
 	year += match[1]
 	// remove year from string
-	resultStr += match[3].toLowerCase()
+	resultStr = albumStr.replace(yearRegex, '').toLowerCase()
 } else resultStr += albumStr.toLowerCase()
 
 // remove extra chars
 resultStr = resultStr.replace(/[\(\)\[\]]/g, ' ').trim()
-resultStr = resultStr.replace(/[-_.]/g, ' ').trim()
+resultStr = resultStr.replace(/[-_.|]/g, ' ').trim()
 
 const artistRegex = new RegExp(artistStr)
 if (artistRegex.test(resultStr)) {
@@ -29,7 +30,7 @@ if (artistRegex.test(resultStr)) {
 
 resultStr = resultStr
 	.split(' ')
-	.map(word => {
+	.map((word, i, arr) => {
 		if (
 			(word == 'to') |
 			(word == 'in') |
@@ -40,12 +41,17 @@ resultStr = resultStr
 		) {
 			return word
 		} else {
-			return String.prototype.concat(word.charAt(0).toUpperCase(), word.slice(1))
+			return String.prototype.concat(
+				word.charAt(0).toUpperCase(),
+				word.slice(1)
+			)
 		}
 	})
+	.filter((word) => word !== '')
 	.join(' ')
 
 resultStr = resultStr.charAt(0).toUpperCase() + resultStr.slice(1)
+console.log(resultStr)
 
 let final = ''
 if (year) {
@@ -56,4 +62,4 @@ if (year) {
 
 fs.renameSync(path.resolve(albumAbs), final)
 
-// STILL NEED TO ADDRESS THE DIRECTIONALITY OF WHERE THE YEAR IS IN THE FILENAME. IF YEAR IS AT END, THIS ALGORITHM WON'T WORK.
+// final consideration is to remove adds for blogs, although this is only a minor concern as it's not too common
